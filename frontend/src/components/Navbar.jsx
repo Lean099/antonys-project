7333f6b42b5b3ad24912d5b3c5941e39d1bc84d8
Enzo Gutierrez <enzogutierrez@github.com>
2025-09-23 23:16:09 -0300

    se agrego componente Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import { React } from 'react';
+import logo from '../assets/logo.png';
+import burgerlogo from '../assets/burgerlogo.png';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav
+        class="navbar rounded-bottom border border-dark border border-1"
+        style={{ background: '#E8DFCA' }}
+      >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img
+              src={burgerlogo}
+              alt=""
+              style={{ width: '50px', height: '50px', marginLeft: '10px' }}
+            />
+            <span
+              class="ms-3"
+              style={{ fontSize: '14px', fontFamily: 'Roboto' }}
+            >
+              Burguer, Pizzas and Lomos
+            </span>
+          </a>
+          <div class="d-flex">
+            <form class="d-flex position-relative" role="search">
+              <div className="position-relative" style={{ maxWidth: '300px' }}>
+                <i
+                  className="bi bi-search position-absolute"
+                  style={{
+                    left: '10px',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                    color: '#999',
+                  }}
+                ></i>
+                <input
+                  type="search"
+                  className="form-control ps-5"
+                  placeholder="Buscar platos..."
+                  aria-label="Buscar"
+                />
+              </div>
+            </form>
+
+            <div class="ms-3">
+              <button
+                type="button"
+                class="btn btn-outline-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Inicio de sesion
+              </button>
+              <button type="button" class="btn mx-1 btn-dark">
+                Registrarse
+              </button>
+              <button type="button" class="btn btn-dark position-relative">
+                <i class="fas fa-shopping-cart"></i> Carrito
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  2<span class="visually-hidden">unread messages</span>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
