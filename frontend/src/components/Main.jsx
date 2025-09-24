5710ee9977215c397f66e9867ba80d83b87bfcbe
Lean099 <leorobledo99@gmail.com>
2025-09-23 23:00:14 -0300

    Se agrego componente main

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+      <div className="container-fluid" style={{ background: '#F0F0F0' }}>
+        <div class="row">
+          <div class="col-12 col-md-6 text-white p-3">
+            
+          </div>
+
+          <div
+            class="col-12 col-md-6 text-white p-4"
+            style={{ background: '#FFFBE9' }}
+          >
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main
