9293a34bd5cd8645b53000bed4f02bd6ddcc460e
Lean099 <leorobledo99@gmail.com>
2025-09-23 23:48:57 -0300

    Se corrigio algo del main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Main from './components/Main'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-     <Navbar/>
-
+      <Navbar/>
       <Main/>
+      <Footer/>
     </>
   )
 }
