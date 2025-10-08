4740052edf22adc3ddd9938948ed79f78bbc3c22
Enzo Gutierrez <enzogutierrez@live.com>
2025-10-07 23:10:51 -0300

    Se agrego la nueva barra de navegacion terminada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-
       <Main/>
       <Footer/>
     </>
